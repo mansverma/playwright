41de3d30e48b436c57e212ae7c64c9a21d6e2c47
mansverma <manvinderverma@aol.com>
2025-09-13 22:19:06 +1000

    fix config file

--- playwright.config.js
+++ playwright.config.js
@@ -33,8 +33,6 @@ module.exports = defineConfig({
   },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
 
   /* Configure projects for major browsers */
   projects: [
